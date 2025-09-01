db3c834d1da11018dfec3fe100bc9a230d5730a7
Neilton Gabriel <153969959+NeiltonGabriel@users.noreply.github.com>
2025-09-01 01:13:51 -0300

    Update Comissionado.java
    Basicamente, refeito

--- Projeto 1/models/Comissionado.java
+++ Projeto 1/models/Comissionado.java
@@ -2,19 +2,29 @@ package br.ufal.ic.p2.wepayu.models;
 
 public class Comissionado extends Empregado{
 
-    private double liquido;;
+    private String liquido;
+    private String comissao;
 
-    public Comissionado(String nome, String endereco, String tipo, double salario, int id){
+    public Comissionado(String nome, String endereco, String tipo, String salario, String id, String comissao){
         super(nome, endereco, tipo, salario, id);
-        setLiquido(salario);
+        this.liquido = salario;
+        this.comissao = comissao;
     }
 
-    public double getLiquido() {
+    public String getLiquido() {
         return liquido;
     }
 
-    public void setLiquido(double liquido) {
+    public void setLiquido(String liquido) {
         this.liquido = liquido;
     }
 
+    public String getComissao() {
+        if (comissao.contains(",")) return comissao;
+        else return String.format("%d,00", Integer.parseInt(comissao));
+    }
+
+    public void setComissao(String comissao) {
+        this.comissao = comissao;
+    }
 }
