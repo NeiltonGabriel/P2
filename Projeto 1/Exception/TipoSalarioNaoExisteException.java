70c38e694e3dd90986ed72d2bbd8f61da1bcfe30
Neilton Gabriel <153969959+NeiltonGabriel@users.noreply.github.com>
2025-09-09 01:05:18 -0300

    Delete Projeto 1/Exception/TipoSalarioNaoExisteException.java

--- Projeto 1/Exception/TipoSalarioNaoExisteException.java
+++
@@ -1,7 +0,0 @@
-package br.ufal.ic.p2.wepayu.Exception;
-
-public class TipoSalarioNaoExisteException extends RuntimeException {
-    public TipoSalarioNaoExisteException(String message) {
-        super(message);
-    }
-}
