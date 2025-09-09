487d788e52a5e131463c6d760d36004151bfe4ed
Neilton Gabriel <153969959+NeiltonGabriel@users.noreply.github.com>
2025-09-09 01:00:55 -0300

    Update Comissionado.java

--- Projeto 1/models/Comissionado.java
+++ Projeto 1/models/Comissionado.java
@@ -1,30 +1,36 @@
 package br.ufal.ic.p2.wepayu.models;
 
-public class Comissionado extends Empregado{
+import br.ufal.ic.p2.wepayu.Exception.ComissaoDeveSerNaoNegativa;
+import br.ufal.ic.p2.wepayu.Exception.ComissaoNaoPodeSerNula;
+import br.ufal.ic.p2.wepayu.Exception.EmpregadoNaoExisteException;
 
-    private String liquido;
+public class Comissionado extends Assalariado{
+
     private String comissao;
 
-    public Comissionado(String nome, String endereco, String tipo, String salario, String id, String comissao){
+    public Comissionado(){
+
+    }
+
+    public Comissionado(String nome, String endereco, String tipo, String salario, String id, String comissao) throws EmpregadoNaoExisteException {
+
         super(nome, endereco, tipo, salario, id);
-        this.liquido = salario;
         this.comissao = comissao;
     }
 
-    public String getLiquido() {
-        return liquido;
-    }
-
-    public void setLiquido(String liquido) {
-        this.liquido = liquido;
-    }
-
     public String getComissao() {
-        if (comissao.contains(",")) return comissao;
+        if (comissao == null) return "";
+        else if (comissao.contains(",")) return comissao;
         else return String.format("%d,00", Integer.parseInt(comissao));
     }
 
     public void setComissao(String comissao) {
         this.comissao = comissao;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(this.getId() + "\nNome: " + this.getNome() + "\nSal├írio: " + this.getSalario()
+                + "\nTipo: " + this.getTipo() + "\nEndere├ºo: " + this.getEndereco() + "\nComiss├úo: " + this.getComissao() + "\n");
     }
 }
